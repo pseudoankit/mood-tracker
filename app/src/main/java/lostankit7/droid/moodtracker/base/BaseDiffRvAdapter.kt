6a1f5eb55869c619579a66b2a8288c7a16471fdb
ankit <kumarankit12353@gmail.com>
2022-01-24 21:46:36 +0530

    minor changes

--- app/src/main/java/lostankit7/droid/moodtracker/base/BaseDiffRvAdapter.kt
+++ app/src/main/java/lostankit7/droid/moodtracker/base/BaseDiffRvAdapter.kt
@@ -1,6 +1,7 @@
 package lostankit7.droid.moodtracker.base
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -12,6 +13,8 @@ abstract class BaseDiffRvAdapter<VB : ViewBinding, T : Any>(
     areItemsSame: (oldItem: T, newItem: T) -> Boolean = { old, new -> old === new },
     areContentsSame: (oldItem: T, newItem: T) -> Boolean = { old, new -> old == new }
 ) : ListAdapter<T, ViewHolder<VB>>(BaseRvComparator<T>(areItemsSame, areContentsSame)) {
+
+    var emptyView : View?= null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<VB> {
         val binding = inflateLayout(LayoutInflater.from(parent.context), parent, false)
@@ -26,6 +29,19 @@ abstract class BaseDiffRvAdapter<VB : ViewBinding, T : Any>(
         bindViewHolder(getItem(position), position, holder.binding)
     }
 
+    class BaseRvComparator<T : Any>(
+        private val areItemsSame: (oldItem: T, newItem: T) -> Boolean,
+        private val areContentsSame: (oldItem: T, newItem: T) -> Boolean
+    ) : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+            return areItemsSame(oldItem, newItem)
+        }
+
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+            return areContentsSame(oldItem, newItem)
+        }
+    }
+
     abstract fun bindViewHolder(item: T, position: Int, binding: VB)
     abstract fun inflateLayout(
         layoutInflater: LayoutInflater, parent: ViewGroup, attachToParent: Boolean
@@ -34,18 +50,5 @@ abstract class BaseDiffRvAdapter<VB : ViewBinding, T : Any>(
     companion object {
         class ViewHolder<VB : ViewBinding>(val binding: VB) :
             RecyclerView.ViewHolder(binding.root)
-
-        class BaseRvComparator<T : Any>(
-            private val areItemsSame: (oldItem: T, newItem: T) -> Boolean,
-            private val areContentsSame: (oldItem: T, newItem: T) -> Boolean
-        ) : DiffUtil.ItemCallback<T>() {
-            override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
-                return areItemsSame(oldItem, newItem)
-            }
-
-            override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
-                return areContentsSame(oldItem, newItem)
-            }
-        }
     }
 }
