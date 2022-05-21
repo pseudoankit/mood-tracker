46b89c53a2864477642068c4c7e11ae822335491
ankit <kumarankit12353@gmail.com>
2022-05-21 23:23:04 +0530

    modules created for navigation

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -6,11 +6,14 @@ object Modules {
     const val entryPresentation = ":entry:entry_presentation"
 
     //homepage
-    const val homeBase = ":home:home_base"
+    const val homeHost = ":home:home_host"
     const val homeUserEntries = ":home:home_user_entries"
     const val homeCalendar = ":home:home_calendar"
     const val homeMore = ":home:home_more"
 
     //common
     const val core = ":core"
+    const val coreNavigation = ":core:core_navigation"
+    const val corePresentation = ":core:core_presentation"
+    const val coreDataDomain = ":core:core_data_domain"
 }
