0062025347e1c6e9acae6b6f80df7f13ecb281e2
Ankit <ankit.kumar1@tokopedia.com>
2022-05-23 12:08:11 +0530

    coreapp component logic unified in core module

--- add_user_entry/entry_presentation/src/main/java/lostankit7/android/entry_presentation/utils/Utils.kt
+++ add_user_entry/entry_presentation/src/main/java/lostankit7/android/entry_presentation/utils/Utils.kt
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.NavHostFragment
-import lostankit7.android.entry_presentation.AddUserEntryHostFragment
-import lostankit7.android.entry_presentation.databinding.CommonActionBarBinding
 import lostankit7.android.entry_presentation.di.DaggerEntryComponent
 import lostankit7.android.entry_presentation.di.EntryComponent
-import lostankit7.droid.moodtracker.core.MyApplication
+import lostankit7.droid.moodtracker.core.utils.coreAppComponent
 
 object Utils {
 
     val FragmentActivity.entryComponent: EntryComponent
         get() = DaggerEntryComponent.builder()
-            .coreAppComponent((application as MyApplication).coreAppComponent).build()
+            .coreAppComponent(application.coreAppComponent).build()
 
     @Suppress("UNCHECKED_CAST")
     fun <F : Fragment> AppCompatActivity.getFragment(fragmentClass: Class<F>): F? {
