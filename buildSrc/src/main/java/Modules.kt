879a63975f434a078b7f6461617c9bbcf9528b12
ankit <kumarankit12353@gmail.com>
2022-06-18 22:04:03 +0530

    restructure

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,15 +1,15 @@
 object Modules {
     const val app = "app"
 
-    const val ENTRY_DATA = ":add_user_entry:entry_data"
-    const val ENTRY_DOMAIN = ":add_user_entry:entry_domain"
-    const val ENTRY_PRESENTATION = ":add_user_entry:entry_presentation"
+    const val ENTRY_DATA = ":feature_user_entry:entry_data"
+    const val ENTRY_DOMAIN = ":feature_user_entry:entry_domain"
+    const val ENTRY_PRESENTATION = ":feature_user_entry:entry_presentation"
 
     //homepage
-    const val HOME_HOST = ":home:home_host"
-    const val HOME_USER_ENTRIES = ":home:home_user_entries"
-    const val HOME_CALENDAR = ":home:home_calendar"
-    const val HOME_MORE = ":home:home_more"
+    const val HOME_HOST = ":feature_home:home_host"
+    const val HOME_USER_ENTRIES = ":feature_home:home_user_entries"
+    const val HOME_CALENDAR = ":feature_home:home_calendar"
+    const val HOME_MORE = ":feature_home:home_more"
 
     //common
     const val CORE = ":common:core"
