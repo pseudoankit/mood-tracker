37c0e0556912d3986d585b5f3bbcd8aea70f8e03
pseudoankit <lostankit7@gmail.com>
2022-11-12 19:55:11 +0530

    updated target and compile sdk

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,8 +1,8 @@
 object ProjectConfig {
     const val APPLICATION_ID = "lostankit7.droid.moodtracker"
-    const val COMPILE_SDK = 31
+    const val COMPILE_SDK = 33
     const val MIN_SDK = 21
-    const val TARGET_SDK = 31
+    const val TARGET_SDK = 33
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 }
